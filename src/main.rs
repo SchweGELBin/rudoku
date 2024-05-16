f394d9a9c9ab878996ded3e0366a6ae07c46e791
SchweGELBin <abramjannikmichael06@gmail.com>
2024-05-16 18:54:16 +0200

    style: remove whitespaces

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn main() {
         [0,0,0,0,0,0,0,0,0]
         [0,0,0,0,0,0,0,0,0]
         [0,0,0,0,0,0,0,0,0]
-    ]; 
+    ];
 
     // Solve grid
     let solved_grid = solve(grid.clone(), 0, 0).
-        expect("Sudoku unsolvable"); 
-    print_grid(solved_grid.clone()); 
+        expect("Sudoku unsolvable");
+    print_grid(solved_grid.clone());
 }
 
 // Checks if the given number is valid in the given cell
@@ -47,7 +47,7 @@ fn check_validity(grid: Grid<u8>, row: u8, col: u8, num: u8) -> bool {
 }
 
 // Solves the sudoku recursively
-fn solve(mut grid: Grid<u8>, mut row: u8, mut col: u8) -> Option<Grid<u8>> { 
+fn solve(mut grid: Grid<u8>, mut row: u8, mut col: u8) -> Option<Grid<u8>> {
     // Handle overflow
     if col >= 9 {
         col = 0;
@@ -58,21 +58,21 @@ fn solve(mut grid: Grid<u8>, mut row: u8, mut col: u8) -> Option<Grid<u8>> {
         return Some(grid);
     }
     // Skip set cells
-    if grid.get(row, col) > Some(&0) { 
+    if grid.get(row, col) > Some(&0) {
         return solve(grid.clone(), row, col + 1);
-    } 
+    }
     // Test cell
     for i in 1..=9 {
         if check_validity(grid.clone(), row, col, i) {
             // Set element to i
-            let grid = set_element(grid.clone(), row, col, i); 
+            let grid = set_element(grid.clone(), row, col, i);
             // Continue recursively
             let solved_grid = solve(grid.clone(), row, col + 1);
             if solved_grid.is_some() {
                 return Some(solved_grid?);
             }
         }
-        // Something prior was invalid 
+        // Something prior was invalid
         grid = set_element(grid, row, col, 0);
     }
     // Else: No possible solution
@@ -101,7 +101,7 @@ fn print_grid(grid: Grid<u8>) {
                 print!("  ");
             }
             print!("{}", grid.get(i, j).
-                   expect("Couldn't print grid: Cell not found")); 
+                   expect("Couldn't print grid: Cell not found"));
         }
         println!();
     }
